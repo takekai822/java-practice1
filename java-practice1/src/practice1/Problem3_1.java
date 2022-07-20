3dad04cbb97573eb50ed229bddb67a7e04aedb55
takekai822 <kaikisoccer78@gmail.com>
2022-07-20 17:10:04 +0900

    練習問題3-1,3-2

---
+++ java-practice1/src/practice1/Problem3_1.java
@@ -0,0 +1,17 @@
+package practice1;
+
+public class Problem3_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int number = (int)(Math.random()*10)+1;
+		System.out.println(number);
+		if (number >= 5) {
+			System.out.println("5以上です");
+		}
+		if (number != 1) {
+			System.out.println("1ではありません");
+		}
+	}
+
+}
