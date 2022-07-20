6ff5692ec32c3238bb0fa48a6dd33527edcaa1f2
takekai822 <kaikisoccer78@gmail.com>
2022-07-20 15:01:03 +0900

    練習問題2-2

---
+++ java-practice1/src/practice1/Problem2_2.java
@@ -0,0 +1,20 @@
+package practice1;
+
+public class Problem2_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 1,b = 2,c = 3,d = 4,e = 5;
+        a += 2;  //  aに2を加える
+        b -= 1;  //  bから1を引く
+        c *= 3;  //  cに3をかける
+        d /= 2;  //  dを2で割る
+        e %= 2;  //  eに、eを2で割った余りを代入する
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+	}
+
+}
