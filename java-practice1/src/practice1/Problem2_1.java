8909ca4c7e98fe53d0fa3e0b2a43fb944cb87293
takekai822 <kaikisoccer78@gmail.com>
2022-07-20 14:57:34 +0900

    練習問題2-1

---
+++ java-practice1/src/practice1/Problem2_1.java
@@ -0,0 +1,17 @@
+package practice1;
+
+public class Problem2_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 5,b = 3;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("a + " + "b" + " = " + (a + b));
+        System.out.println("a - " + "b" + " = " + (a - b));
+        System.out.println("a * " + "b" + " = " + (a * b));
+        System.out.println("a / " + "b" + " = " + (a / b));
+        System.out.println("a % " + "b" + " = " + (a % b));
+	}
+
+}
