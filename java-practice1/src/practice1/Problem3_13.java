2d90900eba8280b4dba9cacf9db961c077e7ded2
takekai822 <kaikisoccer78@gmail.com>
2022-07-20 18:18:28 +0900

    練習問題3-13

--- java-practice1/src/practice1/Problem3_13.java
+++ java-practice1/src/practice1/Problem3_13.java
@@ -6,13 +6,10 @@ public class Problem3_13 {
 		// TODO 自動生成されたメソッド・スタブ
 		int number = (int)(Math.random()*3)+1;
 		if (number == 1) {
-			System.out.println(number);
 			System.out.println("グー");
 		} else if (number == 2) {
-			System.out.println(number);
 			System.out.println("パー");
 		} else {
-			System.out.println(number);
 			System.out.println("チョキ");
 		}
 	}
