05fa2a767e9668988db4a7f4113c8e2eb8021dfd
takekai822 <kaikisoccer78@gmail.com>
2022-07-20 17:34:40 +0900

    練習問題3-8

---
+++ java-practice1/src/practice1/Problem3_8.java
@@ -0,0 +1,16 @@
+package practice1;
+
+public class Problem3_8 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int number = (int)(Math.random()*10)+1;
+		System.out.println(number);
+		if (number != 1) {
+			System.out.println("1ではありません");
+		} else {
+			System.out.println("1です。");
+		}
+	}
+
+}
