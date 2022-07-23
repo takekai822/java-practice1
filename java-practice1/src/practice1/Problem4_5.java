b504b197bd29339949158a3750e468130041d4f6
takekai822 <kaikisoccer78@gmail.com>
2022-07-23 17:14:16 +0900

    練習問題4-5

---
+++ java-practice1/src/practice1/Problem4_5.java
@@ -0,0 +1,12 @@
+package practice1;
+
+public class Problem4_5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 0; i < 4; i++) {
+			System.out.println("a = " + i);
+		}
+	}
+
+}
