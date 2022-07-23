1aaf035c2932e21ecd4413df2cca05dd55210a02
takekai822 <kaikisoccer78@gmail.com>
2022-07-23 17:12:41 +0900

    練習問題4-4

---
+++ java-practice1/src/practice1/Problem4_4.java
@@ -0,0 +1,14 @@
+package practice1;
+
+public class Problem4_4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i = 0;
+		while (i < 4) {
+			System.out.println("a = " + i);
+			i++;
+		}
+	}
+
+}
