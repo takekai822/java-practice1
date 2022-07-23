ca162b4d333e490b2437d6cbdf4ed496395336d9
takekai822 <kaikisoccer78@gmail.com>
2022-07-23 18:06:53 +0900

    練習問題4-15

---
+++ java-practice1/src/practice1/Problem4_15.java
@@ -0,0 +1,16 @@
+package practice1;
+
+public class Problem4_15 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 1; i <= 100; i++) {
+			if (i % 10 == 0) {
+				System.out.println(i);
+			} else {
+				System.out.print(i + " ");
+			}
+		}
+	}
+
+}
