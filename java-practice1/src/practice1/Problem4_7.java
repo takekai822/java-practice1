9141a52549218117666403bda9330bd2ee75729a
takekai822 <kaikisoccer78@gmail.com>
2022-07-23 17:19:23 +0900

    練習問題4-7

---
+++ java-practice1/src/practice1/Problem4_7.java
@@ -0,0 +1,12 @@
+package practice1;
+
+public class Problem4_7 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 3; i >= 0; i--) {
+			System.out.println("a = " + i);
+		}
+	}
+
+}
