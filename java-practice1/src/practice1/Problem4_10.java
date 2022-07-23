6b149bc7e614d8b07494faefe970f671cbd1a213
takekai822 <kaikisoccer78@gmail.com>
2022-07-23 17:41:35 +0900

    練習問題4-10

---
+++ java-practice1/src/practice1/Problem4_10.java
@@ -0,0 +1,14 @@
+package practice1;
+
+public class Problem4_10 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int number;
+		for (int i = 0; i < 5; i++) {
+			number = (int)(Math.random()*100)+1;
+			System.out.println(number);
+		}
+	}
+
+}
