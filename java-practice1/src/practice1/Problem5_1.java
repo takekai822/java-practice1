b871c37c8940910d70220e940f4243f1b4702840
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 14:32:42 +0900

    練習問題5-1 修正

--- java-practice1/src/practice1/Problem5_1.java
+++ java-practice1/src/practice1/Problem5_1.java
@@ -4,13 +4,13 @@ public class Problem5_1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int numbers [] = new int [7];
+		int a [] = new int [7];
 		for (int i = 0; i < 7; i++) {
-			numbers [i] = (int)(Math.random()*10)+1;
+			a [i] = (int)(Math.random()*10)+1;
 			if (i == 6) {
-				System.out.print("a[" + i + "]=" + numbers[i]);
+				System.out.print("a[" + i + "]=" + a[i]);
 			} else {
-				System.out.print("a[" + i + "]=" + numbers[i] + " ");
+				System.out.print("a[" + i + "]=" + a[i] + " ");
 			}
 		}
 	}
