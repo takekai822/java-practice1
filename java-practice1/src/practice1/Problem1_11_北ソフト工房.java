b8d2e6476716a2db59a8c7ad8ec7c6d120542f6a
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 17:05:20 +0900

    北ソフト工房練習問題1-11

---
+++ java-practice1/src/practice1/Problem1_11_北ソフト工房.java
@@ -0,0 +1,11 @@
+package practice1;
+
+public class Problem1_11_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = 22;
+		System.out.println("x/4=" + (x / 4) + ",x%4=" + (x % 4));
+	}
+
+}
