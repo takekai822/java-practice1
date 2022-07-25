cad8a4c51cc30f121c27d1431224080cfc01a474
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 17:32:10 +0900

    北ソフト工房練習問題3-5

---
+++ java-practice1/src/practice1/Problem3_5_北ソフト工房.java
@@ -0,0 +1,18 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class Problem3_5_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		if (x % 2 == 0) {
+			System.out.println("偶数");
+		} else {
+			System.out.println("奇数");
+		}
+	}
+
+}
