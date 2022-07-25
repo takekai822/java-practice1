495ecd7f9720eb05f0f47c9b2b1fe206dba7fcd6
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 17:02:43 +0900

    北ソフト工房練習問題1-10

---
+++ java-practice1/src/practice1/Problem1_10_北ソフト工房.java
@@ -0,0 +1,13 @@
+package practice1;
+
+public class Problem1_10_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = 3;
+		System.out.println("x^1=" + x);
+		System.out.println("x^2=" + x * x);
+		System.out.println("x^3=" + x * x * x);
+	}
+
+}
