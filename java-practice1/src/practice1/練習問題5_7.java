f0ee44de8df9483b35bcf0f9904711093593d09d
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 15:46:22 +0900

    練習問題5-7

---
+++ java-practice1/src/practice1/練習問題5_7.java
@@ -0,0 +1,35 @@
+package practice1;
+
+public class 練習問題5_7 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] data = new int[5];
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			data[i] = (int)(Math.random()*10)+1;
+			if (i == 4) {
+				System.out.println(data[i]);
+			} else {
+				System.out.print(data[i] + " ");
+			}
+			sum += data[i];
+		}
+		System.out.println("合計値：" + sum);
+		System.out.println("平均値：" + sum / 5);
+		System.out.print("平均値より大きい数：");
+		for (int j = 0; j < 5; j++) {
+			if (data[j] >= sum / 5) {
+				System.out.print(data[j] + " ");
+			}
+		}
+		System.out.println();
+		System.out.print("平均値より小さい数：");
+		for (int k = 0; k < 5; k++) {
+			if (data[k] < sum / 5) {
+				System.out.print(data[k] + " ");
+			}
+		}
+	}
+
+}
