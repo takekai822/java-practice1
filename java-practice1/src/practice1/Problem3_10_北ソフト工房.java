38445b77384befad3a7bdc2de620ff2537fbe195
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 18:50:01 +0900

    北ソフト工房練習問題3-10

---
+++ java-practice1/src/practice1/Problem3_10_北ソフト工房.java
@@ -0,0 +1,25 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class Problem3_10_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		if (x < y && x % 2 == 0 && y % 2 == 0) {
+			System.out.println("xはyより小さく、かつ、xとyは共に偶数である。");
+		} else if (x == y && x < 0 && y < 0) {
+			System.out.println( "xとyは等しく、かつ、負の数である。" );
+		} else if (x < y || x % 2 == 0) {
+			System.out.println( "xはyより小さい、または、xは偶数である。" );
+		} else if ((x <= 10 || x >= 100) && (y >= 10 && y <= 100)) {
+			System.out.println( "xは10以下または100以上で、かつ、yは10以上かつ100以下である。" );
+		} else if (!(x < 0 && y < 0)) {
+			System.out.println( "xもyも負の数である、ではない。" );
+		}
+	}
+
+}
