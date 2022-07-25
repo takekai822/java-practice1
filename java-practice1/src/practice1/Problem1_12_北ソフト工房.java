899112ee7fd794c2209f8204b172dd3b2efea22c
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 17:07:18 +0900

    北ソフト工房練習問題1-12

---
+++ java-practice1/src/practice1/Problem1_12_北ソフト工房.java
@@ -0,0 +1,15 @@
+package practice1;
+
+public class Problem1_12_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = 11;
+		x++;
+		x++;
+		System.out.println(x);
+		x--;
+		System.out.println(x);
+	}
+
+}
