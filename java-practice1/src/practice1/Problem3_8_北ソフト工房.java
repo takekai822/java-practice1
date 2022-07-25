f9f6e31b8bf5e7a474909cc4feb0be1938d637c5
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 18:29:27 +0900

    北ソフト工房練習問題3-8

---
+++ java-practice1/src/practice1/Problem3_8_北ソフト工房.java
@@ -0,0 +1,23 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class Problem3_8_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		int midTermExam = sc.nextInt();
+		int finalExam = sc.nextInt();
+		if (midTermExam >= 60 && finalExam >= 60) {
+			System.out.println("合格");
+		} else if ((midTermExam + finalExam) >= 130) {
+			System.out.println("合格");
+		} else if ((midTermExam + finalExam) >= 100 && (midTermExam >= 90 || finalExam >= 90)) {
+			System.out.println("合格");
+		} else {
+			System.out.println("不合格");
+		}
+	}
+
+}
