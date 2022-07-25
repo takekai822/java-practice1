3e8d93eac95f0813185f3b9861948f0970a38860
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 17:17:28 +0900

    北ソフト工房練習問題3-1

---
+++ java-practice1/src/practice1/Problem3_1_北ソフト工房.java
@@ -0,0 +1,15 @@
+package practice1;
+import java.util.Scanner;
+public class Problem3_1_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scanner = new Scanner(System.in);
+		int x = 5;
+		int y = 5;
+		if (x > y) {
+			System.out.println("xはyより大きい");
+		}
+	}
+
+}
