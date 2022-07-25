25c12f499eef5de2737c65420a996dc092407cea
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 17:17:56 +0900

    北ソフト工房練習問題3-1

--- java-practice1/src/practice1/Problem3_1_北ソフト工房.java
+++ java-practice1/src/practice1/Problem3_1_北ソフト工房.java
@@ -5,8 +5,8 @@ public class Problem3_1_北ソフト工房 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Scanner scanner = new Scanner(System.in);
-		int x = 5;
-		int y = 5;
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
 		if (x > y) {
 			System.out.println("xはyより大きい");
 		}
