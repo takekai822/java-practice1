68c54917dc383390c0f8f9a4764e1e3d6811c094
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 14:37:31 +0900

    練習問題5-2

---
+++ java-practice1/src/practice1/Problem5_2.java
@@ -0,0 +1,17 @@
+package practice1;
+
+public class Problem5_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		double[] d = {0.2, -5.1, 3.2, 1.8};
+		for (int i = 0; i < 4; i++) {
+			if (i == 3) {
+				System.out.print("d[" + i + "]=" + d[i]);
+			} else {
+				System.out.print("d[" + i + "]=" + d[i] + " ");
+			}
+		}
+	}
+
+}
