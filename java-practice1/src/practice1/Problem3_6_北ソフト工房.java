95c00348903be7de79a88f4de929b91cb07d025d
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 17:37:00 +0900

    北ソフト工房練習問題3-6

---
+++ java-practice1/src/practice1/Problem3_6_北ソフト工房.java
@@ -0,0 +1,22 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class Problem3_6_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		if (x % 2 == 0 && x >= 0) {
+			System.out.println("正の偶数");
+		} else if (x % 2 == 0 && x < 0) {
+			System.out.println("負の偶数");
+		} else if (x % 2 != 0 && x > 0) {
+			System.out.println("正の奇数");
+		} else {
+			System.out.println("負の奇数");
+		}
+	}
+
+}
