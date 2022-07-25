2453ee14169aaf45d18593126adb7d8d0fe4409e
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 16:55:16 +0900

    北ソフト工房練習問題1-7

---
+++ java-practice1/src/practice1/Problem1_7_北ソフト工房.java
@@ -0,0 +1,16 @@
+package practice1;
+
+public class Problem1_7_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x, y, z;
+		x = 3;
+		y = 8;
+		z = x;
+		x = y;
+		y = z;
+		System.out.println("x=" + x + ",y=" + y);
+	}
+
+}
