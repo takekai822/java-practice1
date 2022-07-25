11900ebc7889e34e445d3cbf61e513f5e58c6323
takekai822 <kaikisoccer78@gmail.com>
2022-07-25 16:59:51 +0900

    北ソフト工房練習問題1-9

---
+++ java-practice1/src/practice1/Problem1_9_北ソフト工房.java
@@ -0,0 +1,13 @@
+package practice1;
+
+public class Problem1_9_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = 3;
+		System.out.println("2x=" + x * 2);
+		System.out.println("3x=" + x * 3);
+		System.out.println("4x=" + x * 4);
+	}
+
+}
