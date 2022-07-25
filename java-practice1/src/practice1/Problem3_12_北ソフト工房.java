db654283afa35d1e5c2afbe84d5e82d637345133
takekai822 <kaikisoccer78@gmail.com>
2022-07-26 08:58:50 +0900

    北ソフト工房練習問題3-12

---
+++ java-practice1/src/practice1/Problem3_12_北ソフト工房.java
@@ -0,0 +1,26 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class Problem3_12_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		System.out.println("寿司を選択");
+		System.out.println("1:マグロ,2:サーモン,3:いくら");
+		int sushi = sc.nextInt();
+		switch(sushi) {
+			case 1:
+				System.out.println("大吉");
+				break;
+			case 2:
+				System.out.println("吉");
+				break;
+			case 3:
+				System.out.println("凶");
+				break;
+		}
+	}
+
+}
