173e0e2fdae0ff41c0b34009846a53a5a13632fb
takekai822 <kaikisoccer78@gmail.com>
2022-07-26 09:53:56 +0900

    北ソフト工房練習問題4-10

---
+++ java-practice1/src/practice1/Problem4_10_北ソフト工房.java
@@ -0,0 +1,16 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class Problem4_10_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		for (int i = 0; i < count; i++) {
+			System.out.print("*");
+		}
+	}
+
+}
