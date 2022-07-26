21c111479db2184f6b74f6e9cb7bc956b7013394
takekai822 <kaikisoccer78@gmail.com>
2022-07-26 09:22:03 +0900

    北ソフト工房練習問題4-4

---
+++ java-practice1/src/practice1/Problem4_4_北ソフト工房.java
@@ -0,0 +1,14 @@
+package practice1;
+
+public class Problem4_4_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int sum = 1;
+		for (int i = 7; i >= 1; i--) {
+			sum *= i;
+		}
+		System.out.println(sum);
+	}
+
+}
