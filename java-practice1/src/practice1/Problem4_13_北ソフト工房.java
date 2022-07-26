fc3afe02248b9914a1301aea604bd057c0e4daa5
takekai822 <kaikisoccer78@gmail.com>
2022-07-26 10:21:15 +0900

    北ソフト工房練習問題4-13

---
+++ java-practice1/src/practice1/Problem4_13_北ソフト工房.java
@@ -0,0 +1,24 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class Problem4_13_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		int count_s = 0;
+		int count_b = 0;
+		while (count_s < 3 && count_b < 4) {
+			System.out.println("ストライク=1 or ボール=2 ？");
+			int judge = sc.nextInt();
+			if (judge == 1) {
+				count_s++;
+			} else if (judge == 2) {
+				count_b++;
+			}
+		}
+		System.out.println(count_b + "ボール," + count_s + "ストライク");
+	}
+
+}
