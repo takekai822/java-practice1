c4f3a6f1984a6a600351c0526d739b61441ac79d
takekai822 <kaikisoccer78@gmail.com>
2022-07-26 09:10:07 +0900

    北ソフト工房練習問題4-2

---
+++ java-practice1/src/practice1/Problem4_2_北ソフト工房.java
@@ -0,0 +1,12 @@
+package practice1;
+
+public class Problem4_2_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(3 * i);
+		}
+	}
+
+}
