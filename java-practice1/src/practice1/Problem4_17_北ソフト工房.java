ec4bb034d6f950dbb9c5e517ea3093bf53ec2928
takekai822 <kaikisoccer78@gmail.com>
2022-07-26 11:40:14 +0900

    北ソフト工房練習問題4-17

---
+++ java-practice1/src/practice1/Problem4_17_北ソフト工房.java
@@ -0,0 +1,15 @@
+package practice1;
+
+public class Problem4_17_北ソフト工房 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.printf("%d×%d=%2d ",i, j, i * j);
+			}
+			System.out.println();
+		}
+	}
+
+}
