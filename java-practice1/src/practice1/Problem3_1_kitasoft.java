1894dadcc6ecda379824d8fbedb479758662f4c0
takekai822 <kaikisoccer78@gmail.com>
2022-07-30 09:59:34 +0900

    命名変更

--- java-practice1/src/practice1/Problem3_1_kitasoft.java
+++ java-practice1/src/practice1/Problem3_1_kitasoft.java
@@ -1,6 +1,6 @@
 package practice1;
 import java.util.Scanner;
-public class Problem3_1_北ソフト工房 {
+public class Problem3_1_kitasoft {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
